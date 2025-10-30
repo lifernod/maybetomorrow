606069e266b08f27f92883c619d09630d6fb0eaf
liferno <lifernod@gmail.com>
2025-10-30 20:16:19 +0700

    frontend: done footer, changed default font

--- frontend/src/components/footer.tsx
+++ frontend/src/components/footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
-        <footer>
-            <h1>Footer</h1>
+        <footer class={"fixed bottom-0 w-full h-8 flex items-center justify-center"}>
+            <p>Сделано с &#129505;️ студентами ТПУ, 2025 г.</p>
         </footer>
     )
 }
