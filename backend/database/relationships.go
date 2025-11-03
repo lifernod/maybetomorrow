c34e88284e36e5042411b62e292cc8768b7ff7a4
Cluster <cassettecluster@gmail.com>
2025-11-03 10:45:14 +0700

    backend. Fix: добавил и изменил теги для json, db

--- backend/database/relationships.go
+++ backend/database/relationships.go
@@ -3,16 +3,16 @@ package database
 import "github.com/google/uuid"
 
 type EventsToDay struct {
-	EventID int `json:"event_id"`
-	DayID   int `json:"day_id"`
+	EventID int `json:"event_id" db:"event_id"`
+	DayID   int `json:"day_id" db:"day_id"`
 }
 
 type DaysToMonths struct {
-	DayID   int `json:"day_id"`
-	MonthID int `json:"month"`
+	DayID   int `json:"day_id" db:"day_id"`
+	MonthID int `json:"month_id" db:"month_id"`
 }
 
 type UsersToMonths struct {
-	UserID  uuid.UUID `json:"user_id"`
-	MonthID int       `json:"months"`
+	UserID  uuid.UUID `json:"user_id" db:"user_id"`
+	MonthID int       `json:"month_id" db:"month_id"`
 }
--- backend/database/structures.go
+++ backend/database/structures.go
@@ -12,25 +12,25 @@ const (
 )
 
 type User struct {
-	UserID    uuid.UUID `json:"user_id"`
-	Username  string    `json:"username"`
-	PasswordT string    `json:"password_t"`
+	UserID    uuid.UUID `json:"user_id" db:"user_id"`
+	Username  string    `json:"username" db:"username"`
+	PasswordT string    `json:"password_t" db:"password_t"`
 }
 
 type Event struct {
-	EventID          int    `json:"event_id"`
-	EventName        string `json:"event_name"`
-	EventDescription string `json:"event_description"`
+	EventID          int    `json:"event_id" db:"event_id"`
+	EventName        string `json:"event_name" db:"event_name"`
+	EventDescription string `json:"event_description" db:"event_description"`
 }
 
 type Day struct {
-	DayID     int     `json:"day_id"`
-	DayNumber byte    `json:"day_number"`
-	DayType   DayType `json:"day_type"`
+	DayID     int     `json:"day_id" db:"day_id"`
+	DayNumber byte    `json:"day_number" db:"day_number"`
+	DayType   DayType `json:"day_type" db:"day_type"`
 }
 
 type Month struct {
-	MonthID     int  `json:"month_id"`
-	MonthNumber byte `json:"month_number"`
-	YearNumber  byte `json:"year_number"`
+	MonthID     int  `json:"month_id db:"month_id""`
+	MonthNumber byte `json:"month_number" db:"month_number"`
+	YearNumber  byte `json:"year_number" db:"year_number"`
 }
