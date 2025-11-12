baeae0bdfa17e9cd8fe4c1b95035cff34be44197
Psychoz <mr.vladik2007@gmail.com>
2025-11-12 21:27:11 +0700

    backend: создана заготовка получения ивентов по id дня

--- backend/handlers/day_handlers.go
+++ backend/handlers/day_handlers.go
@@ -8,9 +8,17 @@ func GetDayById(c *fiber.Ctx) error {
 }
 
 func GetEventsByDayId(c *fiber.Ctx) error {
-	resp := [3]ResponseEvent{}
-	return c.JSON(resp)
+	events := [3]ResponseEvent{}
+	//dayId := c.Params("id")
+	//events, err := database.GetEventsByDayID(dayId)
+
+	//if err != nil {
+	//	return fmt.Errorf("Failed to get events by day id %d: %w", dayId, err)
+	//}
+
+	return c.JSON(events)
 }
+
 func CreateDay(c *fiber.Ctx) error {
 	day := new(ResponseDay)
 
